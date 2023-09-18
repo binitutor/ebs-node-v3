6a133f9228bcc845208528179fa33d1f121b719b
BiniTutor <binitutor1@gmail.com>
2023-09-18 19:03:31 -0400

    added line to dispose db conn

--- app.js
+++ app.js
@@ -31,22 +31,34 @@ conn.connect((err) => {
 app.get("/", (req, res) => res.send("Server is running..."));
 
 app.get('/api/items', (req, res) => {
-    let sql = "SELECT * FROM items";
-    // let query = conn.query(sql, (err, results) => {
-    //     if(err) throw err;
-    //     res.send(apiResponse(results))
+    // conn.connect(function(err) {
+    //     if (err) throw err;
+    //     conn.query("SELECT * FROM customers", function (err, result, fields) {
+    //       if (err) throw err;
+    //       console.log(result);
+    //       res.send(apiResponse(result))
+    //     });
+    //     conn.end();
     // });
 
-    conn.query(sql, (err, results) => {
-        // if(err) throw err;
-        // res.send(apiResponse(results))
-        if(err) {
-            res.send(apiResponse(err))
-        }else{
-            res.send(apiResponse(results))
-        }
-        
+    let sql = "SELECT * FROM items";
+
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(apiResponse(results))
+        conn.end();
     });
+
+    // conn.query(sql, (err, results) => {
+    //     // if(err) throw err;
+    //     // res.send(apiResponse(results))
+    //     if(err) {
+    //         res.send(apiResponse(err))
+    //     }else{
+    //         res.send(apiResponse(results))
+    //     }
+        
+    // });
 
     // let results = '{"status":200,"error":null,"response":[{"id":1,"title":"first title","body":"this is body"},{"id":2,"title":"second title","body":"this is body"}]}'
     // res.send(apiResponse(results))
